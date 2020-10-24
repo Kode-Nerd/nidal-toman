23215667197de50b1507c5541b65c88155d981e5
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-10-24 21:30:46 +0700

    update i18n plugin

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -6,11 +6,17 @@ Vue.use(VueI18n)
 export default ({ app, store }) => {
   // Set i18n instance on app
   app.i18n = new VueI18n({
-    locale: 'de',
-    fallbackLocale: 'de',
+    locale: store.state.locale,
+    fallbackLocale: 'en',
     messages: {
-      en: require(`~/assets/locales/en/treatment.json`),
-      de: require(`~/assets/locales/de/treatment.json`),
+      en: {
+        ...require(`~/assets/locales/en/welcome.json`),
+        ...require(`~/assets/locales/en/treatment.json`),
+      },
+      de: {
+        ...require(`~/assets/locales/de/welcome.json`),
+        ...require(`~/assets/locales/de/treatment.json`),
+      },
     },
   })
 }
