d045417f8ba3ef5f9fe42e86784eb52b74338a11
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-10-24 15:36:44 +0700

    adding treatment components

--- components/Treatments.vue
+++ components/Treatments.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="treatments-container">
-    <div id="welcome" class="treatments-main pa-6">TREATMENTS</div>
+    <div id="treatments" class="treatments-main pa-6">
+      <v-row class="treatments-main-logo"></v-row>
+      <v-row class="full-height">
+        <v-tabs
+          v-model="tab"
+          background-color="transparent"
+          :color="$vuetify.theme.themes.light.primary"
+          right
+        >
+          <v-tab v-for="label in labels" :key="label">{{ label }}</v-tab>
+        </v-tabs>
+      </v-row>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      tab: 0,
+      labels: [
+        'Welcome',
+        'Treatments',
+        'Info Desk',
+        'Dr. Nidal Toman',
+        'Contact',
+      ],
+    }
+  },
+}
 </script>
 
 <style>
@@ -17,5 +42,14 @@ export default {}
 .treatments-main {
   width: 100%;
   height: 200%;
+}
+.treatments-main-logo {
+  height: 0%;
+}
+.full-height {
+  height: 100%;
+}
+.full-width {
+  width: 100%;
 }
 </style>
