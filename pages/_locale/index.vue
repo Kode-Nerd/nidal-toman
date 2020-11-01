f4dc896aab99a6ab03208573d6318a9665526ffd
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-11-01 15:10:36 +0700

    merging

--- pages/_locale/index.vue
+++ pages/_locale/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <!-- <Welcome />
-    <Treatments /> -->
+    <Welcome />
+    <Treatments />
     <TreatmentDetail />
   </div>
 </template>
 
 <script>
-// import TreatmentDetail from '../TreatmentDetail/TreatmentDetail'
-// import Welcome from '~/components/Welcome'
+import Treatments from '~/components/Treatments'
+import Welcome from '~/components/Welcome'
 import TreatmentDetail from '~/components/TreatmentDetail/TreatmentDetail'
 
 export default {
   components: {
-    // Welcome,
-    // Treatments,
+    Welcome,
+    Treatments,
     TreatmentDetail,
   },
 }
