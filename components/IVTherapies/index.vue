a453e93484d4397f80b32f016b996932d3f95935
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-12-12 21:15:58 +0700

    added ivtherapies page

---
+++ components/IVTherapies/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-img
+    :src="require('~/assets/_ivtherapies.jpeg')"
+    width="100%"
+    height="100vh"
+  >
+    <v-row :style="logoContainer" justify="center" align="center">
+      <v-img
+        :style="logoStyle"
+        contain
+        :src="require(`~/assets/_B'lance.png`)"
+        height="80vh"
+      ></v-img>
+    </v-row>
+  </v-img>
+</template>
+
+<script>
+export default {
+  computed: {
+    logoContainer() {
+      return {
+        width: '100%',
+        height: '100%',
+      }
+    },
+    logoStyle() {
+      return {
+        opacity: '40%',
+      }
+    },
+  },
+}
+</script>
+
+<style></style>
