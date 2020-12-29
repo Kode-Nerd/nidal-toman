be89fd7334351d740eba3d7ffd2fec37160bc9db
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-12-29 22:09:57 +0700

    added logo svg and debug page

---
+++ pages/_locale/debug/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <CustomSVG
+    :src="require('~/assets/main-logo.svg')"
+    width="300px"
+    height="120px"
+    :color="themes.light.logo"
+  />
+</template>
+
+<script>
+import CustomSVG from '~/components/Global/CustomSVG'
+export default {
+  components: {
+    CustomSVG,
+  },
+}
+</script>
+
+<style></style>
