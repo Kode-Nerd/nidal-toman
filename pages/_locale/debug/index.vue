af8f84a6ac258ec931bdf086df4249252158ce0d
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-12-29 23:35:17 +0700

    debug

--- pages/_locale/debug/index.vue
+++ pages/_locale/debug/index.vue
@@ -1,19 +1,28 @@
 <template>
   <div v-if="true" />
-  <CustomSVG
+  <!-- <CustomSVG
     v-else
     :src="require('~/assets/main-logo.svg')"
     width="300px"
     height="120px"
     :color="themes.light.logo"
-  />
+  /> -->
+  <div v-else>
+    <v-img
+      contain
+      position="bottom right"
+      :src="require('~/assets/adobe.jpeg')"
+      height="100px"
+      width="300px"
+    ></v-img>
+  </div>
 </template>
 
 <script>
-import CustomSVG from '~/components/Global/CustomSVG'
+// import CustomSVG from '~/components/Global/CustomSVG'
 export default {
   components: {
-    CustomSVG,
+    // CustomSVG,
   },
   computed: {
     themes() {
