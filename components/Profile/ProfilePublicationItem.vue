8d1dcc3ce5ade8be25859ebafb3978432574bf48
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-01-10 21:32:24 +0700

    change type

--- components/Profile/ProfilePublicationItem.vue
+++ components/Profile/ProfilePublicationItem.vue
@@ -29,7 +29,7 @@ export default {
       default: '',
     },
     index: {
-      type: String,
+      type: Number,
       default: '',
     },
   },
--- components/Profile/ProfilePublishedSummary.vue
+++ components/Profile/ProfilePublishedSummary.vue
@@ -27,7 +27,7 @@ export default {
       default: '',
     },
     index: {
-      type: String,
+      type: Number,
       default: '',
     },
   },
