d6096983ecc1880d734b6afd3b4799ec5e42f136
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-01-19 23:16:26 +0700

    created image block component

---
+++ components/Global/ImageBlock.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="container"
+    :style="{
+      height,
+      backgroundImage: `url(${src})`,
+      backgroundSize: 'contain',
+    }"
+  >
+    <img class="image" :src="src" alt="no-image" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '100px',
+    },
+  },
+  computed: {
+    containerStyle() {
+      return {
+        height: this.height,
+        backgroundImage: `url(${this.src})`,
+        backgroundSize: 'contain',
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  display: inline-block;
+}
+.image {
+  height: 100%;
+  object-fit: contain;
+  visibility: hidden;
+}
+</style>
