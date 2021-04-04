a4398e896a176c9dfc5b4243d54df67a15aff677
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-04-04 22:07:43 +0700

    added floated button

--- components/Mobile/Procedures/index.vue
+++ components/Mobile/Procedures/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <MobileView></MobileView>
+  <MobileView>
+    <v-fab-transition>
+      <v-btn
+        v-show="!hidden"
+        elevation="2"
+        color="primary3"
+        dark
+        fab
+        fixed
+        top
+        left
+        small
+        @click="drawer = !drawer"
+        ><v-icon small>fas fa-bars</v-icon></v-btn
+      >
+    </v-fab-transition>
+  </MobileView>
 </template>
 
 <script>
@@ -8,6 +24,16 @@ export default {
   components: {
     MobileView,
   },
+  computed: {
+    drawer: {
+      set(val) {
+        this.$store.commit('SET_SIDENAV', val)
+      },
+      get() {
+        return this.$store.state.sideNav
+      },
+    },
+  }
 }
 </script>
 
